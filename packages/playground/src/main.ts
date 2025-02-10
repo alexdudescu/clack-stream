cbf0ed6bf3d58c0833b296329f6d8293b74e83d4
Alex Dudescu <alexandru.dudescu@uipath.com>
2025-02-10 08:56:44 +0200

    feat: add playground project workspace

---
+++ packages/playground/src/main.ts
@@ -0,0 +1,3 @@
+import { sayHello} from "@alexdudescu/clack-streams"
+
+sayHello();
---
+++ packages/playground/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+const isDev = process.env.npm_lifecycle_event === "dev";
+
+export default defineConfig({
+    clean: true,
+    entry: ["./src/main.ts"],
+    format: ["cjs"],
+    minify: !isDev,
+    target: "node20",
+    outDir: "dist",
+    sourcemap: true,
+    onSuccess: isDev ? "node ./dist/main.js" : undefined,
+});
+
